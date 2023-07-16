1040313184f5fb7ed5a982347c6077ec9ef885d7
Aditya Rohman <adityarhmaan@Adityas-MacBook-Pro.local>
2023-07-16 12:37:31 +0700

    update section 2 chapter 2

--- Section2CollectionTypes.playground/Pages/Chapter2.xcplaygroundpage/Contents.swift
+++ Section2CollectionTypes.playground/Pages/Chapter2.xcplaygroundpage/Contents.swift
@@ -4,8 +4,7 @@ import Foundation
 
 // Chapter 1: Collection Iteration with Closures
 
-exercise()
-
+// Exercises
 func exercise() {
   let names = ["Ella", "Freya", "Indira", "Lulu"]
   print(names)
@@ -37,5 +36,66 @@ func exercise() {
   }
   print(adultsNames)
 }
+
+exercise()
+
+// Challenges
+
+// #1
+func repeatTask(times: Int, task: () -> Void) {
+  for _ in 0..<times {
+    task()
+  }
+}
+
+repeatTask(times: 10) {
+  print("Swift Apprentice is a great book!")
+}
+
+// #2
+func mathSum(length: Int, series: (Int) -> Int) -> Int {
+  var sum = 0
+  for i in 1...length {
+    sum += series(i)
+  }
+  return sum
+}
+
+let sumOf10SquareNumbers = mathSum(length: 10) { number in
+  number * number
+}
+print(sumOf10SquareNumbers)
+
+func fibonacci(_ number: Int) -> Int {
+  if number < 1 { return 0 }
+  if number <= 2 { return 1 }
+  
+  return fibonacci(number - 1) + fibonacci(number - 2)
+}
+
+let sumOf10FibonacciNumbers = mathSum(length: 10) { number in
+  fibonacci(number)
+}
+print(sumOf10FibonacciNumbers)
+
+// #3
+let appRatings = [
+  "Calendar Pro": [1, 5, 5, 4, 2, 1, 5, 4],
+  "The Messenger": [5, 4, 2, 5, 4, 1, 1, 2],
+  "Socialise": [2, 1, 2, 2, 1, 2, 4, 2]
+]
+
+var averageRatings: [String: Double] = [:]
+appRatings.forEach { appName, ratings in
+  averageRatings[appName] = Double(ratings.reduce(0, { $0 + $1 })) / Double(ratings.count)
+}
+print(averageRatings)
+
+let appsWithHighAverageRating = averageRatings.filter { _, rating in
+  rating > 3
+}.map { appName, rating in
+  appName
+}
+print(appsWithHighAverageRating)
 
 //: [Next](@next)
